7da5c67562ec4204b18ba1967c7c666b712bec34
Chris Reams <chrisreams91@gmail.com>
2022-12-05 21:15:39 -0600

    part 2

--- day4/2.js
+++ day4/2.js
@@ -1,2 +1,36 @@
 const { readInput } = require("../util");
 const input = readInput("./input.txt");
+
+const findRange = (first, second) => {
+  const range = [];
+  range.push(first);
+  while (range[range.length - 1] !== second) {
+    range.push(range[range.length - 1] + 1);
+  }
+  return range;
+};
+
+const formattedInput = input.map((line) => {
+  const splitPairs = line.split(",");
+  const pairRanges = splitPairs.map((pair) => {
+    const splitNumbers = pair.split("");
+    const splitIndex = splitNumbers.indexOf("-");
+
+    const firstNums = splitNumbers.slice(0, splitIndex);
+    const secondNums = splitNumbers.slice(splitIndex + 1, splitIndex.length);
+
+    const range = findRange(
+      Number(firstNums.join("")),
+      Number(secondNums.join(""))
+    );
+
+    return range;
+  });
+
+  return pairRanges[0].some((num) => pairRanges[1].find((x) => x === num));
+});
+
+console.log(formattedInput);
+
+const finalCount = formattedInput.filter(Boolean).length;
+console.log(finalCount);
